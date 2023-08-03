7779e00281ec8ce52dc26505769cc73ed2840b5f
Alex Katlein <dev@vemilyus.com>
2023-08-03 15:53:40 +0200

    feat: Implemented ExprEvaluathor

--- src/main/kotlin/com/vemilyus/treeCalc/eval/ExprEvaluathor.kt
+++ src/main/kotlin/com/vemilyus/treeCalc/eval/ExprEvaluathor.kt
@@ -1,9 +1,32 @@
 package com.vemilyus.treeCalc.eval
 
+import com.vemilyus.treeCalc.model.AddSubOp
 import com.vemilyus.treeCalc.model.Expr
+import com.vemilyus.treeCalc.model.MulDivOp
 
 class ExprEvaluathor {
-    fun eval(expr: Expr): Double {
-        TODO()
-    }
+    fun eval(expr: Expr): Double =
+        when (expr) {
+            is Expr.AddSub -> {
+                val left = eval(expr.left)
+                val right = eval(expr.right)
+
+                when (expr.op) {
+                    AddSubOp.Add -> left + right
+                    AddSubOp.Sub -> left - right
+                }
+            }
+
+            is Expr.MulDiv -> {
+                val left = eval(expr.left)
+                val right = eval(expr.right)
+
+                when (expr.op) {
+                    MulDivOp.Mul -> left * right
+                    MulDivOp.Div -> left / right
+                }
+            }
+
+            is Expr.Term -> expr.value
+        }
 }
